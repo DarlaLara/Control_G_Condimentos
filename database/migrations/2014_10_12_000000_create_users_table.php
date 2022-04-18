3020e6656df7bd23cec9b65d7160d969e8c3be29
DarlaLara <laradar987@gamil.com>
2022-04-18 11:22:10 -0500

    ProyectoFinal

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('usu_id');
+            $table->string('usu_name');
+            $table->string('usu_rol');
+            $table->string('password');
+            $table->string('usu_email')->unique();
+            $table->string('usu_cedula')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+            $table->foreignId('per_id')->refences('per_id')->on('Permisos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
